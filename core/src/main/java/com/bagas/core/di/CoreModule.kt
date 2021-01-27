5f075dd3bcfbd3dcb02143d18c09c2f180c94573
Bagas Hariyanto <bagashyt@gmail.com>
2021-01-27 20:03:35 +0700

    added ssl pinning

--- core/src/main/java/com/bagas/core/di/CoreModule.kt
+++ core/src/main/java/com/bagas/core/di/CoreModule.kt
@@ -8,6 +8,7 @@ import com.bagas.core.data.source.remote.RemoteDataSource
 import com.bagas.core.data.source.remote.network.ApiService
 import com.bagas.core.domain.repository.IMovieAppRepository
 import com.bagas.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -28,10 +29,15 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
